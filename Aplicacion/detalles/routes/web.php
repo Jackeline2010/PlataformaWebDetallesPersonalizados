46128844ad3f06f37ff91a0ac7da7a985c608911
Cristian Espinoza <christtian.3x@gmail.com>
2025-07-01 23:59:39 -0500

    Configuración de estilos / rutas

--- Aplicacion/detalles/routes/web.php
+++ Aplicacion/detalles/routes/web.php
@@ -2,17 +2,43 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return view('/shop/home');
+})->name('home');
+
+Route::get('/admin', function () {
+    return view('/admin/dashboard');
 });
+
+Route::get('/login', function () {
+    return view('/shop/customer/login');
+})->name('login');
+
+Route::get('/forgot', function () {
+    return view('/shop/customer/forgot');
+})->name('forgot');
+
+Route::get('/register', function () {
+    return view('/shop/customer/register');
+})->name('register');
+
+
+Route::get('/about', function () {
+    return view('/shop/pages/about');
+})->name('about');
+
+Route::get('/contact', function () {
+    return view('/shop/pages/contact');
+})->name('contact');
+
+Route::get('/terms', function () {
+    return view('/shop/pages/terms');
+})->name('terms');
+
+Route::get('/products', function () {
+    return view('/shop/products/products');
+})->name('products');
+
+Route::get('/cart', function () {
+    return view('/shop/checkout/cart');
+})->name('cart');
