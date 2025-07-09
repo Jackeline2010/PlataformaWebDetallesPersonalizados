dfe3ae1321e0bf3dec4677ac34ef773696852e1b
cpespinozalo <cpespinoza@utpl.edu.ec>
2025-07-09 15:32:13 -0500

    Creación de seccion de productos

--- Aplicacion v10/detalles/resources/views/shop/checkout/cart.blade.php
+++ Aplicacion v10/detalles/resources/views/shop/checkout/cart.blade.php
@@ -146,13 +146,6 @@
                 <a href="#" class="text-base font-medium text-gray-900 hover:underline dark:text-white">Ramo de Girasoles y Rosas - Estilo Nocturno</a>
 
                 <div class="flex items-center gap-4">
-                  <button type="button" class="inline-flex items-center text-sm font-medium text-gray-500 hover:text-gray-900 hover:underline dark:text-gray-400 dark:hover:text-white">
-                    <svg class="me-1.5 h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12.01 6.001C6.5 1 1 8 5.782 13.001L12.011 20l6.23-7C23 8 17.5 1 12.01 6.002Z" />
-                    </svg>
-                    Agregar a Favoritos
-                  </button>
-
                   <button type="button" class="inline-flex items-center text-sm font-medium text-red-600 hover:underline dark:text-red-500">
                     <svg class="me-1.5 h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                       <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18 17.94 6M18 18 6.06 6" />
